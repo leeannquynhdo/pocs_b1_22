743fc52edb9e2daa3c9a7245c0c832a7932a3839
Leeann Quynh Do <leeannquynhdo@Leeanns-MBP.webspeed.dk>
2022-11-05 22:53:54 +0100

    show random image when user reaches daily goal pt 2

--- app/src/main/java/com/example/puk/GoToFacebook.java
+++ app/src/main/java/com/example/puk/GoToFacebook.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class GoToFacebook extends AppCompatActivity {
 
@@ -29,6 +32,28 @@ public class GoToFacebook extends AppCompatActivity {
         fbProgress.setMax(fbAvgTime);
         fbProgress.setProgress(fbTimeMinute);
 
+        int[] images = {R.drawable.img01,
+                R.drawable.img02,
+                R.drawable.img03,
+                R.drawable.img04,
+                R.drawable.img05,
+                R.drawable.img06,
+                R.drawable.img07,
+                R.drawable.img08,
+                R.drawable.img09,
+                R.drawable.img10,
+                R.drawable.img11,
+                R.drawable.img12,
+                R.drawable.img13,
+                R.drawable.img14,
+                R.drawable.img15,
+                R.drawable.img16,
+                R.drawable.img17,
+                R.drawable.img18,
+                R.drawable.img19,
+                R.drawable.img20};
+        Random rand = new Random();
+
         TextView lTextView = (TextView) findViewById(R.id.fb_motivator);
         StringBuilder lStringBuilder = new StringBuilder();
 
@@ -42,12 +67,21 @@ public class GoToFacebook extends AppCompatActivity {
 
         if (fbTimeMinute >= fbAvgTime) {
             lStringBuilder.append("You did it! Well done!");
+            lStringBuilder.append(System.getProperty("line.separator"));
+            lStringBuilder.append(System.getProperty("line.separator"));
+            lStringBuilder.append("You have unlocked the hidden image");
+            ImageView lImageView = (ImageView) findViewById(R.id.fb_image);
+            lImageView.setImageResource(images[rand.nextInt(images.length)]);
+
         } else {
             lStringBuilder.append("You can do it!");
             lStringBuilder.append(System.getProperty("line.separator"));
             lStringBuilder.append("Only ");
             lStringBuilder.append(fbAvgTime-fbTimeMinute);
             lStringBuilder.append(" minutes to go");
+            lStringBuilder.append(System.getProperty("line.separator"));
+            lStringBuilder.append(System.getProperty("line.separator"));
+            lStringBuilder.append("Reach the daily goal to unveil the hidden image!");
         }
 
         lTextView.setText(lStringBuilder.toString());
